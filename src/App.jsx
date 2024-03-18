093b292692fb2bf182ab9e6b9fc57c5a371ca4e6
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-03-18 23:53:56 +0600

    creating navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+// import DaisyNav from "./components/daisyNav/DaisyNav";
 
 function App() {
   return (
     <>
+      {/* <DaisyNav></DaisyNav> */}
+      <Navbar></Navbar>
       <h1 className="bg-rose-600 text-3xl">Vite + React</h1>
     </>
   );
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,23 @@
+const Navbar = () => {
+  const routes = [
+    { id: 1, path: "/", name: "Home" },
+    { id: 2, path: "/about", name: "About" },
+    { id: 3, path: "/services", name: "Services" },
+    { id: 4, path: "/contact", name: "Contact" },
+    { id: 5, path: "*", name: "NotFound" },
+  ];
+
+  return (
+    <nav>
+      <ul className="md:flex ">
+        {routes.map((route) => (
+          <li key={route.id} className="mr-8">
+            <a href={route.path}>{route.name}</a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
